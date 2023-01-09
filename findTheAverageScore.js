d51ab6aee8e47c7f63871d85a2beac8fade20cd1
PhuocMai31 <Phuwowsc@gmail.com>
2023-01-09 10:14:59 +0700

    second

--- findTheAverageScore.js
+++ findTheAverageScore.js
@@ -2,65 +2,65 @@ let students = [
     {
         name: "Ha",
         gender: 'female',
-        poin: 8
+        point: 8
     },
     {
         name: "Huy",
         gender: 'male',
-        poin: 9
+        point: 9
     },
     {
         name: "Hung",
         gender: 'male',
-        poin: 7
+        point: 7
     },
     {
         name: "Phuong",
         gender: 'female',
-        poin: 6
+        point: 6
     },
     {
         name: "Huyen",
         gender: 'female',
-        poin: 10
+        point: 10
     },
     {
         name: "Long",
         gender: 'male',
-        poin: 5
+        point: 5
     },
     {
         name: "Luan",
         gender: 'male',
-        poin: 10
+        point: 10
     },
     {
         name: "Linh",
         gender: 'female',
-        poin: 8
+        point: 8
     }
 
 ];
 
 function findTheAverage(students){
     var total = 0;
-    var index = 0;
+    var count = 0;
     for (let i = 0; i < students.length; i++){
         if (students[i].gender === "male"){
-            total = total + students[i].poin
-            index++;
+            total = total + students[i].point
+            count++;
         }
     }
-    console.log(`the average score of the boys: ${total/index}`)
+    console.log(`the average score of the boys: ${total/count}`)
     var total = 0;
-    var index = 0;
+    var count = 0;
     for (let i = 0; i < students.length; i++){
         if (students[i].gender === "female"){
-            total = total + students[i].poin
-            index++;
+            total = total + students[i].point
+            count++;
         }
     }
-    console.log(`the average score of the girl: ${total/index}`)
+    console.log(`the average score of the girl: ${total/count}`)
 
 }
 findTheAverage(students)
